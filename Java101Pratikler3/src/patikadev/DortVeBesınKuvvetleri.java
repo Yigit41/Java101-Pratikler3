a2a33aaac1bf3e9c5a398283bfb05153b150c5f5
Yigit41 <yigit.lisans@outlook.com>
2022-11-23 02:49:08 +0300

    Create DortVeBesınKuvvetleri.java

---
+++ Java101Pratikler3/src/patikadev/DortVeBesınKuvvetleri.java
@@ -0,0 +1,31 @@
+package patikadev;
+
+import java.util.Scanner;
+
+public class DortVeBesınKuvvetleri {
+
+	public static void main(String[] args) {
+		int number;
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Please Enter Positive Number");
+		
+		number = input.nextInt();
+		
+		System.out.println("Forces of four");
+
+		for(int i=1;i<number;i*=4 ) 
+		{
+			
+			System.out.println(i);
+		}
+		System.out.println("Forces of five");
+		for(int i=1;i<number;i*=5 ) 
+		{
+			
+			System.out.println(i);
+		}
+	}
+
+}
