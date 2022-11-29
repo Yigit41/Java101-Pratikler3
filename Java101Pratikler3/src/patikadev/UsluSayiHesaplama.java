105132f025cd1fd1a109e576564c7c5a9b8c955d
Yigit41 <yigit.lisans@outlook.com>
2022-11-29 03:41:25 +0300

    Create UsluSayiHesaplama.java

---
+++ Java101Pratikler3/src/patikadev/UsluSayiHesaplama.java
@@ -0,0 +1,32 @@
+package patikadev;
+
+import java.util.Scanner;
+
+public class UsluSayiHesaplama {
+
+	public static void main(String[] args) {
+		
+		int n,k,result=1;
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Please enter the number to be exponentially :  ");
+		n = input.nextInt();
+		
+		System.out.println("Please enter the power number :  ");
+		
+		k = input.nextInt();
+		
+		
+		for(int i=0;i<k;i++) 
+		{
+			
+			result *=n;
+			
+			
+		}
+		
+		System.out.println("Result : "+result);
+		
+	}
+
+}
