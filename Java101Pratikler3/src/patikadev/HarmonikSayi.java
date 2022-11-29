a9123542b7a75073393667d03c68a040d2f620df
Yigit41 <yigit.lisans@outlook.com>
2022-11-29 04:07:33 +0300

    Create HarmonikSayi.java

---
+++ Java101Pratikler3/src/patikadev/HarmonikSayi.java
@@ -0,0 +1,28 @@
+package patikadev;
+
+import java.util.Scanner;
+
+public class HarmonikSayi {
+
+	public static void main(String[] args) {
+		int number;
+		double result = 0;
+		
+		Scanner input = new Scanner(System.in);
+
+		
+		System.out.println("Please enter a positive number  :  ");
+		
+		number = input.nextInt();
+		
+		
+		 for (double i = 1; i <= number; i++) {
+
+			 result += 1 / i;
+	        }
+	        System.out.println(result);
+		
+		
+	}
+
+}
